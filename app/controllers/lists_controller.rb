79830f86d4096c63ef32ab517f694689c3d2ac23
Amal Rabbani <130931565+AmalRabbani@users.noreply.github.com>
2023-06-28 21:05:04 +0200

    views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,27 @@
 class ListsController < ApplicationController
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @bookmarks = @list.bookmarks
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
   end
 end
+
+private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
